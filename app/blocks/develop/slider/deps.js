30c7cc5e051a07e532ffbd8482dc18984f495b5c
Andrey Karamnoff <karamnoff@gmail.com>
2020-01-17 19:17:38 +0300

    add markup & styles for slider

---
+++ app/blocks/develop/slider/deps.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  modules: [
+    {
+      from: `node_modules/swiper/js`,
+      inject: [`swiper.min.js`],
+    },
+    {
+      from: `node_modules/swiper/css`,
+      inject: [`swiper.min.css`],
+    },
+  ],
+
+};
