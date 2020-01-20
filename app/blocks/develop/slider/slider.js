194784efbcc480e42310381dc75c0bc2315632d3
Andrey Karamnoff <karamnoff@gmail.com>
2020-01-20 11:37:35 +0300

    make refactoring for slider

--- app/blocks/develop/slider/slider.js
+++ app/blocks/develop/slider/slider.js
@@ -1,30 +1,31 @@
 /* global Swiper */
 
 document.addEventListener(`DOMContentLoaded`, () => {
-  const swiperContainer = document.querySelector(
-    `#slider .swiper-container`
-  );
-
-  if (swiperContainer) {
-    const swiper = new Swiper(swiperContainer, {
-      noSwiping: true,
-      autoHeight: true,
-      // autoplay: {
-      //   delay: 4000
-      // },
-      pagination: {
-        el: `.swiper-pagination`,
-        type: `bullets`,
-        clickable: `true`,
-      },
-      breakpoints: {
-        1023: {
-          autoplay: {
-            delay: 3000
-          },
-          noSwiping: false,
+  const mainSlider = new Swiper(`.about`, {
+    loop: true,
+    speed: 500,
+    spaceBetween: 10,
+    noSwiping: false,
+    // autoplay: {
+    //   delay: 3000
+    // },
+    pagination: {
+      el: `.swiper-pagination`,
+      type: `bullets`,
+      clickable: `false`
+    },
+    breakpoints: {
+      1024: {
+        noSwiping: true,
+        pagination: {
+          el: `.swiper-pagination`,
+          type: `bullets`,
+          clickable: true
         }
+        // autoplay: {
+        //   delay: 4000
+        // }
       }
-    });
-  }
+    }
+  });
 });
