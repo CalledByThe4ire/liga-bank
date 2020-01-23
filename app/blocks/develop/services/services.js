547b93f1d32fc4527dab7b0901c24328c1bf191c
Andrey Karamnoff <karamnoff@gmail.com>
2020-01-23 19:17:40 +0300

    fix services' markup & styles, add services' js for tab-desk

--- app/blocks/develop/services/services.js
+++ app/blocks/develop/services/services.js
@@ -1,9 +1,13 @@
+/* global Swiper */
+/* eslint-disable no-unused-vars */
+
 document.addEventListener(`DOMContentLoaded`, () => {
+  // tab' switch behaviour
   const links = document.querySelectorAll(`#services .services__link`);
   const sections = document.querySelectorAll(`#services .tab-service`);
 
   const eventHandler = (event, activeElement) => {
-    const {type, keyCode} = event;
+    const { type, keyCode } = event;
 
     if (type === `click`) {
       event.preventDefault();
@@ -15,24 +19,24 @@ document.addEventListener(`DOMContentLoaded`, () => {
     }
   };
 
-  [`click`, `keyup`].forEach((event) => {
-    links.forEach((link) => {
-      link.addEventListener(event, (e) => eventHandler(e, link));
+  [`click`, `keyup`].forEach(event => {
+    links.forEach(link => {
+      link.addEventListener(event, e => eventHandler(e, link));
     });
   });
 
   const removeActiveTab = () => {
-    links.forEach((link) => {
+    links.forEach(link => {
       link
         .closest(`.services__list-item`)
         .classList.remove(`services__list-item--active`);
     });
-    sections.forEach((section) => {
+    sections.forEach(section => {
       section.classList.remove(`tab-service--active`);
     });
   };
 
-  const addActiveTab = (link) => {
+  const addActiveTab = link => {
     link
       .closest(`.services__list-item`)
       .classList.add(`services__list-item--active`);
@@ -40,4 +44,45 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const matchingSection = document.querySelector(href);
     matchingSection.classList.add(`tab-service--active`);
   };
+
+  // slider
+  const breakpoint = window.matchMedia(`only screen and (max-width:1024px)`);
+  let servicesSlider = null;
+
+  const enableSlider = () => {
+    servicesSlider = new Swiper(`#services`, {
+      loop: true,
+      speed: 500,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 3000
+      },
+      pagination: {
+        el: `.swiper-pagination`,
+        type: `bullets`,
+        clickable: `false`
+      }
+    });
+  };
+
+  const breakpointChecker = () => {
+    if (breakpoint.matches) {
+      enableSlider();
+      Array.from(sections).forEach((section) =>
+        section.classList.contains(`tab-service--active`)
+          ? section.classList.remove(`tab-service--active`)
+          : section
+      );
+    } else {
+      if (servicesSlider) {
+        servicesSlider.destroy();
+        Array.from(sections).forEach((section, index) =>
+          index === 0 ? section.classList.add(`tab-service--active`) : section
+        );
+      }
+    }
+  };
+
+  breakpointChecker();
+  breakpoint.addListener(breakpointChecker);
 });
