5045f928c0ce5a58a1278d06b52cf806304673ec
Andrey Karamnoff <karamnoff@gmail.com>
2020-02-24 12:34:28 +0300

    make refactoring for project' global styles

--- app/blocks/develop/calculator/deps.js
+++ app/blocks/develop/calculator/deps.js
@@ -1,9 +1,12 @@
 module.exports = {
-
   modules: [
     {
       from: `node_modules/imask/dist`,
       inject: [`imask.min.js`],
+    },
+    {
+      from: `node_modules/input-range-scss`,
+      import: [`_inputrange.scss`],
     },
   ],
 
