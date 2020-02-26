64b58e1b32cc06780fd6ab0f48a4b78fdb0862b8
Andrey Karamnoff <karamnoff@gmail.com>
2020-02-26 18:31:39 +0300

    fix localstorage issues for calculator

--- app/blocks/develop/calculator/calculator.js
+++ app/blocks/develop/calculator/calculator.js
@@ -826,11 +826,11 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const registrationFormData = localStorage.getItem(`registrationFormData`)
       ? JSON.parse(localStorage.getItem(`registrationFormData`))
       : {
-        fullName: creditRegistrationFullNameInput.unmaskedValue || ``,
-        tel: Number(creditRegistrationTelInput.unmaskedValue) || ``,
-        email: creditRegistrationEmailInput.unmaskedValue || ``
+        fullName: creditRegistrationFullNameInput.unmaskedValue,
+        tel: Number(creditRegistrationTelInput.unmaskedValue),
+        email: creditRegistrationEmailInput.unmaskedValue
       };
-    const {fullName, tel, email} = registrationFormData;
+    const {fullName = ``, tel = ``, email = ``} = registrationFormData;
 
     if (creditRegistrationFullNameInput) {
       creditRegistrationFullNameInput.value = fullName;
