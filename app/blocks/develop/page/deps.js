09317cb3348430b79afdf010824ebc00fdfea9ce
Andrey Karamnoff <karamnoff@gmail.com>
2020-03-04 07:51:37 +0300

    add smooth-scroll to page

--- app/blocks/develop/page/deps.js
+++ app/blocks/develop/page/deps.js
@@ -4,5 +4,9 @@ module.exports = {
       from: `node_modules/animate.css`,
       import: [`animate.min.css`],
     },
+    {
+      from: `node_modules/smooth-scroll`,
+      inject: [`smooth-scroll.polyfills.min.js`],
+    },
   ],
 };
